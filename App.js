64a913f836e9265a5fb85e908d8e937d070cd398
Brent Rusnell <blrusnell@gmail.com>
2019-06-19 13:17:14 -0500

    getting data back from api

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import {Text, View, AppRegistry, TextInput} from 'react-native';
+import { API_KEY, API_URL } from 'react-native-dotenv';
+import axios from 'axios';
+
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input: 'ENTER ZIP CODE'
+      input: 'ENTER ZIP CODE',
+      currentWeather: {},
+
     }
+  }
+
+  handleZipCodeInput = () => {
+    axios.get(`${API_URL}zip=${this.state.input}&APPID=${API_KEY}`)
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        currentWeather: response.data
+      })
+    })
+    .catch(error => console.log(error))
   }
 
   render() {
@@ -16,24 +33,29 @@ export default class App extends Component {
         <View style={{
           flexDirection: 'column',
           backgroundColor: 'powderblue',
-          height: 200,
+          height: 160,
           padding: 30,
         }}>
           <TextInput
           style={{height: 70, borderColor: 'gray', borderWidth: 1, marginTop: 70}}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-          defaultValue={this.state.input}
+          onChangeText={(value) => {
+            console.log(this.state.input)
+            this.setState({
+              input: value
+            }
+            )}}
+          value={this.state.input}
           color={'white'}
           fontSize={30}
           fontFamily={'Trebuchet-BoldItalic'}
           borderWidth={0}
           clearTextOnFocus={true}
+          onSubmitEditing={this.handleZipCodeInput}
           />
         </View>
         <View style={{
           backgroundColor:'skyblue',
-          height: 500
+          height: 600
         }}>
 
         </View>
---
+++ server.js
@@ -0,0 +1,18 @@
+// const express = require('express')
+// const app = express()
+// const port = 19000
+// const request = require('request');
+// const bodyParser = require('body-parser')
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//     console.log(req.params);
+//     if (err) {
+//         console.log(err);
+//         res.end();
+//     }
+//     res.send()
+// })
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
